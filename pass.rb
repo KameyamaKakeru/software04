1fef7fb6738c6633aa4817c7eb9a4ecc53ad9b8c
Kameyama <2211140012n@ed.fuk.kindai.ac.jp>
2024-10-15 23:28:28 +0000

    Initial commit

---
+++ pass.rb
@@ -0,0 +1,14 @@
+require 'bcrypt'
+
+#ユーザ登録のアクション
+signup_password = BCrypt::Password.create("my password")
+puts signup_password
+
+#ログインのアクション
+if User.find_by(uid: params[:uid])
+    login_password = BCrypt::Password.new(signup_password)
+    if login_password == "my password" #←login_password.==(“my password”)
+        p "ログイン成功"
+    end
+    
+end
